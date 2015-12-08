95f2d37a60b3b8dfda6bfb7e4196d28b7f4f65be
Zach Meza <zachmmeza@gmail.com>
2015-12-07 21:38:09 -0800

    Configuring GraceNote metadata

--- project.py
+++ project.py
@@ -14,28 +14,70 @@ def main():
 	three_runs_next_output = []
 
 
-	total = 0
+	fetchGraceNoteData = False
+	if fetchGraceNoteData:
+		metadata_file = open('metadata.json')
+	else:
+		metadata_file = open('new_metadata.json')
+	gracenote_metadata = json.load(metadata_file)
+	metadata_file.close()
+
+
+	
+	for elem in train_data_json:
+		if elem not in gracenote_metadata:
+			print "not there"
+			print elem
+			print "\n\n"
+	raw_input()
+
+	# for elem in gracenote_metadata:
+	# 	print elem
+	# raw_input()
+
+	# new_metadata = {}
+	# for elem in gracenote_metadata['tracks']:
+	# 	song = elem["track_title"]
+	# 	artist = elem["album_artist_name"]
+	# 	new_metadata[song + " ::: " + artist] = elem
+	# json.dump(new_metadata, open('new_metadata.json', 'w'))
+
+
 	for elem in train_data_json:
 		song, artist = elem.split(":::")
-		print artist
-		print song
-		metadata = getGraceNoteMetadata(artist, song)
-		song_name metadata['track_title']
-		artist_name = metadata['album_artist_name']
+		if fetchGraceNoteData:
+			track_metadata = getGraceNoteMetadata(artist, song)
+		else:
+			if elem in gracenote_metadata:
+				track_metadata = gracenote_metadata[elem]
+			else:
+				print song, artist
+				print "not there...\n"
+				continue
+		song_name = track_metadata['track_title']
+		artist_name = track_metadata['album_artist_name']
 
-		if artist.strip() == artist_name.strip() and song.strip() == song_name.strip():
-			total+=1
-		#print metadata
+		tempo = track_metadata['tempo']
+		print song_name
+		print artist_name
+		print tempo
 		raw_input()
+
+		if fetchGraceNoteData and artist.strip() == artist_name.strip() and song.strip() == song_name.strip():
+			gracenote_metadata['tracks'].append(track_metadata)
+
 		positions_list = []
 		for x in train_data_json[elem]:
 			positions_list.append(int(x[u'position']))
 		if len(positions_list) >= 4:
 			for i in range(len(positions_list)-4):
-				all_three_runs.append([positions_list[i], positions_list[i+1], positions_list[i+2]])
+				all_three_runs.append([positions_list[i], positions_list[i+1], positions_list[i+2], ])
 				three_runs_next_output.append(positions_list[i+3])	
-	print total
+	print "finished lol"
 	raw_input()
+
+	if fetchGraceNoteData:
+		json.dump(metadata, open('metadata.json', 'w'))
 
 	clf = linear_model.Ridge(alpha=0.4)
 	clf.fit(all_three_runs, three_runs_next_output)
